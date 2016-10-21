542c0e37acbb81958aba55fc2603628ca0562f87
Kunuk Nykjær <kunukn@gmail.com>
2016-10-21 13:43:42 +0200

    puzzle logic example

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -11,16 +11,50 @@ import CubeOne from './cube-one';
 
 //log('App running');
 
+const states = {};
+
+function statechangeCallback(eventName, payload) {
+    states[payload.cube.id] = payload.currentStateCode;
+    checkForComplete();
+}
+
+function initCallback(eventName, payload) {}
+
+const bodyWrapperEl = qs('.body-wrapper');
+
+function checkForComplete() {
+
+
+    let keys = Object.keys(states);
+    let isComplete = false;
+
+    if (keys.length >= 4) {
+        isComplete = true;        
+        keys.forEach((key, index, array) => {
+            if (states[key] !== 'uf') {
+                isComplete = false;                
+            }
+        });
+    }
+
+    if (isComplete)
+        bodyWrapperEl.classList.add('with-background-image');
+    else {
+        bodyWrapperEl.classList.remove('with-background-image');
+    }
+}
+
+
 window.cubeOne = window.cubeOne || [];
 
 let cube = new CubeOne({
     cubeComponent: byId('cubeone-component-1'),
     infoComponent: qs('.js-state-info-1'),
 });
-//cube.addCallbackForEvent('init', (eventName, payload) => {log(eventName), log(payload)});
+cube.addCallbackForEvent('init', initCallback);
+cube.addCallbackForEvent('statechange', statechangeCallback);
 cube.init();
 cube.setToRandomState();
-cube.addCallbackForEvent('statechange', (eventName, payload) => {log(eventName), log(payload)});
 window.cubeOne.push(cube);
 
 //----------
@@ -28,9 +62,10 @@ cube = new CubeOne({
     cubeComponent: byId('cubeone-component-2'),
     infoComponent: qs('.js-state-info-2'),
 });
+cube.addCallbackForEvent('init', initCallback);
+cube.addCallbackForEvent('statechange', statechangeCallback);
 cube.init();
 cube.setToRandomState();
-cube.addCallbackForEvent('statechange', (eventName, payload) => {log(eventName), log(payload)});
 window.cubeOne.push(cube);
 
 //----------
@@ -38,9 +73,10 @@ cube = new CubeOne({
     cubeComponent: byId('cubeone-component-3'),
     infoComponent: qs('.js-state-info-3'),
 });
+cube.addCallbackForEvent('init', initCallback);
+cube.addCallbackForEvent('statechange', statechangeCallback);
 cube.init();
 cube.setToRandomState();
-cube.addCallbackForEvent('statechange', (eventName, payload) => {log(eventName), log(payload)});
 window.cubeOne.push(cube);
 
 //----------
@@ -48,10 +84,15 @@ cube = new CubeOne({
     cubeComponent: byId('cubeone-component-4'),
     infoComponent: qs('.js-state-info-4'),
 });
+cube.addCallbackForEvent('init', initCallback);
+cube.addCallbackForEvent('statechange', statechangeCallback);
 cube.init();
 cube.setToRandomState();
-cube.addCallbackForEvent('statechange', (eventName, payload) => {log(eventName), log(payload)});
 window.cubeOne.push(cube);
+
+
+
+
 
 //----------
 log('cubeOne is available in console');
