9f250d44fc32374bbda12201cd3f0e0e1cb7cac2
Kunuk Nykjær <kunukn@gmail.com>
2016-10-27 09:39:05 +0200

    device info reorder and hover

--- webpack.config.js
+++ webpack.config.js
@@ -77,9 +77,9 @@ module.exports = {
             test: /\.scss$/,
             //exclude: /node_modules/,
             include: [getPath('src/assets/styles')],
-            //loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?browsers=last 1 versions!sass-loader'),
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
-    }, {
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader?browsers=last 1 versions!sass-loader'),
+            //loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+        }, {
             test: /\.css$/,
             include: getPath('node_modules/normalize.css'),
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
